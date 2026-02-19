bd0603473a6789ee348e7952e7e5787cc9b922b6
dasdware <info@dasdware.de>
2026-02-19 22:38:18 +0100

    Changed: Updated main.c to contain checkboxes as well

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <raylib.h>
 #include <dui/environment.h>
 #include <dui/button.h>
+#include <dui/checkbox.h>
 
 #define ARENA_IMPLEMENTATION
 #include <arena.h>
@@ -12,11 +13,8 @@
 
 int main(void)
 {
-    InitWindow(800, 600, "DUI");
+    InitWindow(50*16, 50*9, "DUI");
     SetTargetFPS(60);
-
-    DUI_Environment* env = dui_env();
-    env->primary_shades.palette = &PALETTE_SKY;
 
     while (!WindowShouldClose())
     {
@@ -26,19 +24,40 @@ int main(void)
             {
                 dui_lay_begin_stack(.gap = 2);
                 {
-                    dui_label("Buttons");
+                    dui_lay_begin_stack(.gap = 2, .direction = DIRECTION_HORIZONTAL, .item_size = 200);
+                    {
+                        dui_lay_begin_stack(.gap = 2);
+                        {
+                            dui_label("Buttons");
 
-                    dui_button(.caption = "Default");
-                    dui_button(.caption = "Secondary", .kind = DUI_SECONDARY);
-                    dui_button(.caption = "Primary", .kind = DUI_PRIMARY);
-                    dui_button(.caption = "Disabled", .disabled = true);
+                            dui_button(.caption = "Primary", .kind = DUI_PRIMARY);
+                            dui_button(.caption = "Secondary", .kind = DUI_SECONDARY);
+                            dui_button(.caption = "Default");
+                            dui_button(.caption = "Disabled", .disabled = true);
+                        }
+                        dui_lay_end();
 
-                    dui_label("Labels");
-                    dui_label("Default");
-                    dui_label("Secondary", .kind = DUI_SECONDARY);
-                    dui_label("Primary", .kind = DUI_PRIMARY);
+                        dui_lay_begin_stack(.gap = 2);
+                        {
+                            dui_label("Labels");
+                            dui_label("Primary", .kind = DUI_PRIMARY);
+                            dui_label("Secondary", .kind = DUI_SECONDARY);
+                            dui_label("Default");
+                        }
+                        dui_lay_end();
 
-
+                        dui_lay_begin_stack(.gap = 2);
+                        {
+                            dui_label("Checkboxes");
+                            static bool primary_checked = true;
+                            dui_checkbox(.caption = "Primary", .kind = DUI_PRIMARY, .checked = &primary_checked);
+                            dui_checkbox(.caption = "Secondary", .kind = DUI_SECONDARY);
+                            dui_checkbox(.caption = "Default");
+                            dui_checkbox(.caption = "Disabled", .disabled = true);
+                        }
+                        dui_lay_end();
+                    }
+                    dui_lay_end();
 
                     if (dui_button(.caption = "Exit", .kind = DUI_PRIMARY, .opposite = true))
                     {
@@ -49,7 +68,6 @@ int main(void)
             }
             dui_lay_end();
         }
-
         dui_env_end();
     }
     CloseWindow();
