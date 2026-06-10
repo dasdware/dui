a869a7750447b4bce8f706779157d1b26a41bacd
dasdware <info@dasdware.de>
2026-06-10 20:57:35 +0200

    Add float animation

--- src/dui/animation.c
+++ src/dui/animation.c
@@ -1,10 +1,12 @@
 #include <dui/animation.h>
 
-DUI_ColorAnimation dui_ca_create(const Color initial_color) {
+#include "raymath.h"
+
+DUI_ColorAnimation dui_ca_create(const Color initial_value) {
     return (DUI_ColorAnimation){
-        .from = initial_color,
-        .to = initial_color,
-        .current = initial_color,
+        .from = initial_value,
+        .to = initial_value,
+        .current = initial_value,
         .finished = true,
     };
 }
@@ -30,4 +32,36 @@ void dui_ca_update(DUI_ColorAnimation* animation) {
 
     const double t = (current_time - animation->start_time) / animation->duration;
     animation->current = ColorLerp(animation->from, animation->to, t);
+}
+
+DUI_FloatAnimation dui_fa_create(const float initial_value) {
+    return (DUI_FloatAnimation){
+        .from = initial_value,
+        .to = initial_value,
+        .current = initial_value,
+        .finished = true,
+    };
+}
+
+void dui_fa_start(DUI_FloatAnimation* animation, const float to, const double duration) {
+    animation->from = animation->current;
+    animation->to = to;
+    animation->start_time = GetTime();
+    animation->duration = duration;
+    animation->finished = false;
+}
+
+void dui_fa_update(DUI_FloatAnimation* animation) {
+    if (animation->finished) {
+        return;
+    }
+
+    const double current_time = GetTime();
+    if (current_time > animation->start_time + animation->duration) {
+        animation->finished = true;
+        return;
+    }
+
+    const double t = (current_time - animation->start_time) / animation->duration;
+    animation->current = Lerp(animation->from, animation->to, t);
 }
--- src/dui/animation.h
+++ src/dui/animation.h
@@ -13,8 +13,21 @@ typedef struct {
     bool finished;
 } DUI_ColorAnimation;
 
-DUI_ColorAnimation dui_ca_create(Color initial_color);
+DUI_ColorAnimation dui_ca_create(Color initial_value);
 void dui_ca_start(DUI_ColorAnimation* animation, Color to, double duration);
 void dui_ca_update(DUI_ColorAnimation* animation);
+
+typedef struct {
+    float from;
+    float to;
+    float current;
+    double start_time;
+    double duration;
+    bool finished;
+} DUI_FloatAnimation;
+
+DUI_FloatAnimation dui_fa_create(float initial_value);
+void dui_fa_start(DUI_FloatAnimation* animation, float to, double duration);
+void dui_fa_update(DUI_FloatAnimation* animation);
 
 #endif //DUI_ANIMATION_H
